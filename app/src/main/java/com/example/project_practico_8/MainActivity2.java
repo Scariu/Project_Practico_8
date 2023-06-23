09f76f9d70a9dbf336cf9be6ccd0912078cb5555
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-22 22:17:15 -0400

    Se agrega función a botón URL y se agregan imagenes al diseño. Función de botón Second Activity definida en commit anterior.

--- app/src/main/java/com/example/project_practico_8/MainActivity2.java
+++ app/src/main/java/com/example/project_practico_8/MainActivity2.java
@@ -2,7 +2,11 @@ package com.example.project_practico_8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        initListeners();
+    }
+
+    private void initListeners() {
+        Button buttonUrl = findViewById(R.id.buttonIntentUrl);
+        buttonUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent implicitIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://kotlindoc.blogspot.com/2019/10/android-log-con-timber.html"));
+                startActivity(implicitIntent);
+            }
+        });
     }
 }
